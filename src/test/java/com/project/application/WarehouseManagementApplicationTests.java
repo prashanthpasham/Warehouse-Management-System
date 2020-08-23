d52e69eebd4ccff5b6ee16fd69c8613978fb9b76
prashanth <prashanthpasham@users.noreply.github.com>
2020-08-23 10:08:31 +0530

    Project directories uploded

---
+++ src/test/java/com/project/application/WarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
